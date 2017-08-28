ce6bd3f11bfb31acc7640f64e8368eec90338073
Jack Musial <jacek@musi.al>
2017-08-28 21:14:15 +0200

    Filled Main.java

--- src/al/musi/jacek/Main.java
+++ src/al/musi/jacek/Main.java
@@ -1,10 +1,40 @@
 package al.musi.jacek;
 
+import al.musi.jacek.model.Person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Person>
+        List<Person> persons = new ArrayList<>();
+
+        try (
+            BufferedReader reader =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    Main.class.getResourceAsStream("people.txt")));
+            Stream<String> stream = reader.lines();
+            ) {
+            stream.map(line -> {
+                String[] s = line.split(" ");
+                Person p = new Person(s[0].trim(), Integer.parseInt(s[1]));
+                persons.add(p);
+                return p;
+            }).forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        Stream<Person> stream = persons.stream();
+        stream.filter(person -> person.getAge() >= 20)
+                .min(Comparator.comparing(Person::getAge));
     }
 }
